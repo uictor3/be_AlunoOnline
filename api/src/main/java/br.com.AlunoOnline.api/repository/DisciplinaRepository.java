8db0769386fe7c84e07c76312ba15455c79d32c9
wictor costa <142526733+uictor3@users.noreply.github.com>
2024-06-04 21:31:24 -0300

    Delete api/src/main/java/br.com.AlunoOnline.api directory

--- api/src/main/java/br.com.AlunoOnline.api/repository/DisciplinaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.AlunoOnline.api.repository;
-
-import br.com.AlunoOnline.api.model.Disciplina;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
-}
